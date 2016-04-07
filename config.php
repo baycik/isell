57fb3127b5222989a6a44599db3e954e6efef30b
Baycik <Baycik@192.168.3.2>
2016-04-07 22:20:42 +0300

    remove email prefs

--- config.php
+++ config.php
@@ -5,17 +5,4 @@ define('BAY_DB_PASS','');
 define('BAY_DB_NAME','isell3');
 define('BAY_DB_MAIN','isell3');
 define('BAY_COOKIE_NAME','isell3');
-define('BAY_UPDATE_URL','http://github.com/baycik/isell3/archive/MultiActiveComp.zip');
-
-define('BAY_SMTP_SERVER','smtp.googlemail.com');
-define('BAY_SMTP_USER','nilsonkrim@gmail.com');
-define('BAY_SMTP_PASS','makhablinch');
-define('BAY_SMTP_PORT',465);
-define('BAY_SMTP_CRYPTO','ssl');
-define('BAY_SMTP_SENDER_MAIL','krim@nilson.ua');
-define('BAY_SMTP_PRIVATE_MAIL','bay@nilson.ua');
-define('BAY_SMTP_SENDER_NAME','ООО "Нильсон Крым"');
-
-define('BAY_SMS_SENDER','NILSON.UA');
-define('BAY_SMS_USER','1511_plustrade');
-define('BAY_SMS_PASS','plustrade');
+define('BAY_UPDATE_URL','http://github.com/baycik/isell/archive/master.zip');
