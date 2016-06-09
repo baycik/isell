e999fa35595f2990fdce7b3d23673c52283a24b1
baycik <baycik@baycikNote>
2016-06-09 13:13:31 +0300

    working on expence report

---
+++ application/plugins/Reports_summary_expence/info.php
@@ -0,0 +1,12 @@
+<?php
+    return [
+	'title'=>'Анализ Затрат',
+	'description'=>'Выводит развернутую информацию о затратах',
+	'version'=>'0,9',
+	'group_name'=>'Результаты деятельности',
+	'uri'=>'',
+	'user_level'=>4,
+	'author'=>'baycik',
+	'author_uri'=>'',
+	'licence'=>''
+    ];
