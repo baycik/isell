472f52bc9ec4448f61a0c9024a42a653ec4a117d
Baycik <Baycik@192.168.3.2>
2016-08-02 20:40:18 +0300

    comment changed

--- install/db_update.sql
+++ install/db_update.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `companies_list` 
-ADD COLUMN `price_label` VARCHAR(45) NULL DEFAULT NULL AFTER `curr_code`
+ADD COLUMN `price_label` VARCHAR(45) NULL DEFAULT NULL AFTER `curr_code`;
+ALTER TABLE `price_list` 
+CHANGE COLUMN `label` `label` VARCHAR(45) NOT NULL COMMENT 'Категория цен';
