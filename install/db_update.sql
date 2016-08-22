ff8cd3ff82c1cf723cb40bdb4071a4eae66e1e6e
baycik <baycik@baycikNote>
2016-08-22 17:00:43 +0300

    timestamps to datetime

--- install/db_update.sql
+++ install/db_update.sql
@@ -2,3 +2,14 @@ ALTER TABLE `companies_list`
 ADD COLUMN `price_label` VARCHAR(45) NULL DEFAULT NULL AFTER `curr_code`;
 ALTER TABLE `price_list` 
 CHANGE COLUMN `label` `label` VARCHAR(45) NOT NULL COMMENT 'Категория цен';
+
+
+ALTER TABLE `acc_check_list` 
+CHANGE COLUMN `date` `date` DATETIME NULL DEFAULT NULL ,
+CHANGE COLUMN `value_date` `value_date` DATETIME NULL DEFAULT NULL ,
+CHANGE COLUMN `assumption_date` `assumption_date` DATETIME NULL DEFAULT NULL ,
+CHANGE COLUMN `transaction_date` `transaction_date` DATETIME NULL DEFAULT NULL 
+
+
+ALTER TABLE `event_list` 
+CHANGE COLUMN `event_date` `event_date` DATETIME NULL DEFAULT NULL 
