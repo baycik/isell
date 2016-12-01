0cdf48c7da8cba49a909a6e027a3180b7606a320
Baycik <Baycik@192.168.3.2>
2016-12-01 23:05:50 +0300

    working on vk_api_sync

--- application/plugins/vk_api_sync/vk_api_sync.php
+++ application/plugins/vk_api_sync/vk_api_sync.php
@@ -18,8 +18,10 @@ class vk_api_sync extends PluginManager{
 	$this->market_id=-abs($this->settings->market_id);
 	$this->vk=new Vk($this->settings);
 	header("Content-type: text/plain");
-    }
-    public function marketGetAll(){
+	
+	
+   }
+    private function marketGetAll(){
 	$offset=0;
 	$marketItems=[];
 	do{
@@ -32,7 +34,7 @@ class vk_api_sync extends PluginManager{
 	} while( 1 );
 	return $marketItems;
     }
-    public function marketGetAndStore(){
+    private function marketGetAndStore(){
 	$items=$this->marketGetAll();
 	$Storage=$this->Base->load_model('Storage');
 	return $Storage->json_store('vk_api_sync/downloaded_market_items.json',$items);
@@ -41,7 +43,7 @@ class vk_api_sync extends PluginManager{
      * marketMakeDictionary
      * finds product_code in description and links it with id in market items
      */
-    public function marketMakeDictionary(){
+    private function marketMakeDictionary(){
 	$items=$this->marketGetAll();
 	if( !is_array($items) ){
 	    throw new Exception('No downloaded market items are found');
@@ -54,5 +56,42 @@ class vk_api_sync extends PluginManager{
 	}
 	$Storage=$this->Base->load_model('Storage');
 	return $Storage->json_store('vk_api_sync/market_dictionary.json',$dictionary);
+    }
+    private function uploadProducts($chunk){
+	foreach($chunk as $product){
+	    echo $product->product_code;
+	}
+    }
+    private function uploadChunk(){
+	$limit=10;
+	$offset=$this->Base->svar('vk_api_offset');
+	$Storage=$this->Base->load_model('Storage');
+	$dictionary=(array)$Storage->json_restore('vk_api_sync/market_dictionary.json');
+	$chunk=array_slice($dictionary,$offset,$limit);
+	$this->uploadProducts($chunk);
+	$this->Base->svar('vk_api_offset',$offset+$limit);
+	return 0;
+    }
+    public function syncProducts(){
+	$next_step='';
+	$this_step=$this->Base->svar('vk_api_next_step');
+	switch($this_step){
+	    case 'upload_chunk':
+		$left_count=$this->uploadChunk();
+		$next_step=$left_count?'upload_chunk':'stop';
+		echo '-- Синхронизация товаров. Осталось: '.$left_count;
+		break;
+	    case 'stop':
+		echo 'Синхронизация окончена';
+		$this->Base->svar('vk_api_next_step','');
+		break;
+	    default:
+		//$this->marketMakeDictionary();
+		$next_step='upload_chunk';
+		echo '-- Получен список товаров с ВК';
+		$this->Base->svar('vk_api_offset',0);
+ 	}
+	$this->Base->svar('vk_api_next_step',$next_step);
+	return ' ok';
     }
 }
