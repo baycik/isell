7521b654266ec05a279196f27047772143328d7a
Baycik <Baycik@192.168.3.2>
2016-12-04 20:48:53 +0300

    vk_api_sync release

--- application/plugins/vk_api_sync/vk_api_sync.php
+++ application/plugins/vk_api_sync/vk_api_sync.php
@@ -18,9 +18,7 @@ class vk_api_sync extends PluginManager{
 	$this->market_id=-abs($this->settings->market_id);
 	$this->vk=new Vk($this->settings);
 	header("Content-type: text/plain");
-	
-	
-   }
+    }
     private function marketGetAll(){
 	$offset=0;
 	$marketItems=[];
@@ -34,16 +32,7 @@ class vk_api_sync extends PluginManager{
 	} while( 1 );
 	return $marketItems;
     }
-//    public function marketGetAndStore(){
-//	$items=$this->marketGetAll();
-//	$Storage=$this->Base->load_model('Storage');
-//	return $Storage->json_store('vk_api_sync/downloaded_market_items.json',$items);
-//    }
-    /*
-     * marketMakeDictionary
-     * finds product_code in description and links it with id in market items
-     */
-    public function marketGetAndCombine(){
+    private function marketGetAndCombine(){
 	$items=$this->marketGetAll();
 	if( !is_array($items) ){
 	    throw new Exception('No downloaded market items are found');
@@ -56,56 +45,104 @@ class vk_api_sync extends PluginManager{
 	$Storage=$this->Base->load_model('Storage');
 	return $Storage->json_store('vk_api_sync/market_items_combine.json',$items);
     }
-    private function uploadProducts($chunk){
-	$item=$chunk[0];
+    private function stockRating($product_code,$description){
+	$stock_count=$this->get_value("SELECT product_quantity FROM stock_entries WHERE product_code='{$product_code}'");
+	if( $stock_count>300 ){
+	    $level="★★★★★";
+	} else
+	if( $stock_count>100 ){
+	    $level="★★★★☆";
+	} else
+	if( $stock_count>50 ){
+	    $level="★★★☆☆";
+	} else 
+	if( $stock_count>10 ){
+	    $level="★★☆☆☆";
+	}  else 
+	if( $stock_count>0 ){
+	    $level="★☆☆☆☆";
+	} else {
+	    $level="☆☆☆☆☆ (Под заказ)";
+	}
+	return preg_replace('|Наличие:(.*)$|mi', "Наличие: $level", $description);
+    }
+    private function uploadProduct($item){
+	$company_id=$this->settings->pcomp_id;//company_id for wich we will retrieve prices
+	$usd_ratio=$this->Base->pref('usd_ratio');
+	$price=$this->get_value("SELECT GET_PRICE('{$item->product_code}',$company_id,$usd_ratio)");
+	$description=$this->stockRating($item->product_code, $item->description);
 	
-	
-	//print_r( $item);
-	
-	$product=[
-	    'owner_id'=>$this->market_id,
-	    'item_id'=>$item->id,
-	    'name'=>$item->title,
-	    'description'=>$item->description,
-	    'category_id'=>$item->category->id,
-	    'price'=>666,//$item->price->amount/1000,
-	    'main_photo_id'=>$item->photos[0]->id
-	];
-	
-	$this->vk->market->edit($product);
-	print_r($product);
-	$product=[''];
+	if( !is_numeric($price) ){
+	    echo " Не найден код: ".$item->product_code;
+	    $this->Base->msg("Не найден код: ".$item->product_code);
+	    return true;
+	}
+	if( $price && $price!=$item->price->amount/100 || $description!=$item->description ){
+	    $product=[
+		'owner_id'=>$this->market_id,
+		'item_id'=>$item->id,
+		'name'=>$item->title,
+		'description'=>$description,
+		'category_id'=>$item->category->id,
+		'price'=>$price,
+		'main_photo_id'=>$item->photos[0]->id
+	    ];
+	    
+	    echo $item->product_code;
+	    
+	    try{
+		echo " Обновляем: ".$item->product_code;
+		return $this->vk->market->edit($product);
+	    } catch(Exception $e){
+		echo " Ошибка ВК: ".$e->getMessage();;
+		return false;
+	    }
+	}
+	echo " Не изменился ".$item->product_code;
+	return true;
     }
     public function uploadChunk(){
-	$limit=10;
+	$limit=1;
 	$offset=$this->Base->svar('vk_api_offset');
 	
 	$Storage=$this->Base->load_model('Storage');
 	$items=$Storage->json_restore('vk_api_sync/market_items_combine.json');
 	$chunk=array_slice($items,$offset,$limit);
-	$this->uploadProducts($chunk);
-	
+	foreach($chunk as $item){
+	    if( $this->uploadProduct($item) ){
+		continue;
+	    } else {
+		sleep(1);
+		$this->uploadProduct($item);
+	    }
+	}
 	$this->Base->svar('vk_api_offset',$offset+$limit);
-	return 0;
+	return count($items)-$offset;
     }
     public function syncProducts(){
-	$next_step='';
 	$this_step=$this->Base->svar('vk_api_next_step');
 	switch($this_step){
 	    case 'upload_chunk':
 		$left_count=$this->uploadChunk();
-		$next_step=$left_count?'upload_chunk':'stop';
-		echo '-- Синхронизация товаров. Осталось: '.$left_count;
+		if($left_count>0){
+		    $next_step='upload_chunk';
+		    echo ' -- Осталось: '.$left_count;
+		} else{
+		    $next_step='stop';
+		    echo ' -- Все товары синхронизированы';		    
+		}
 		break;
 	    case 'stop':
-		echo 'Синхронизация окончена';
-		$this->Base->svar('vk_api_next_step','');
+		echo ' Синхронизация окончена';
+		$next_step='';
 		break;
 	    default:
-		//$this->marketMakeDictionary();
+		$this->marketGetAndCombine();
 		$next_step='upload_chunk';
 		echo '-- Получен список товаров с ВК';
 		$this->Base->svar('vk_api_offset',0);
+		$next_step='upload_chunk';
+		break;
  	}
 	$this->Base->svar('vk_api_next_step',$next_step);
 	return ' ok';
