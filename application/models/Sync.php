c32d7715721dd7fdfb8678196e4a9bb3b91aab05
baycik <baycik@DESKTOP-DKE3FOL>
2016-12-12 16:39:30 +0300

    syncer update

--- application/models/Sync.php
+++ application/models/Sync.php
@@ -3,7 +3,7 @@ include "Catalog.php";
 class Sync extends Catalog {
     private $gateway_url = "http://nilsonmag.com/open/?route=module/baycikSync";
     private $login = "Default";
-    private $key = "PeeyPTkk7ptcaWPEocWjJKub199ov8zsGblWGS1RDjZmoDWHJbNkilItjvk7ofHnVeeJz48l9FIUEAuxJ3z3SIqd9lGpvEakqZzV2AizA19epJ1nz1DWXT090ic5BOn4PefoabFCOz19zKhJmUYr8uZ2ol3HDcoyJIFj9tPg2Kyt8DQWVtwBYJZKN2QXhOzdejbMwED6eqt2XcwDvbOx7WDqxpMMMjykmapuzGxY9shietVfoIndLsBKwEvAfgx4";
+    private $key = "NqSnphoicYAYfO5ewh9N6u5qrExwJhui8ZeXRb4E3YJHp28hG4heyx6Q6tz79tqPtojEQ6drB2wZqxIR4zsJOlknjJBoKDXCaIiAwdgWte4k9QUrs3zuRAArGl35Mr5zeDDuzzG19tb6GrgypY8Z8LQVoenz4VbTfuvYoAnn7gkLpwqn4NHGT0OpyUpVAkdGLXshCDD3S3DH7BTwhEuz1wIdqPFCIHBjmxgsRbTVYqCeh0KSVIWhR6awTXUEvpnZCv3a2xIh0MhiQSQjB6bpsAXNBgdqLz5f63LYYfdFudinHfSRbEn8jYV3UkHoSha43nNVqBy8KHqa2HRkcvwwEMVhIWOC8LRIySuWH8KlbDPsZsRvl2eWf1AZv50Y9J4m4lcKCddUgtVkGwSgRmMIxmUIFJs0WD0ptB5zXimbyDgMDdtkwevbVOO21eGs26PCDCJzmo9VQMZEcbMNCZPjpZrX8jEbaji2kdBSDy2S33Q6sjSWkw7MZSNH0a4RkpsDxZVBgfvuo9qwicrsaxP522DbwzemTtc2yflhyxk8Kp5AvDChz8ltUH19kOCDo2HuG1QORlJlN9lcbiPox1LtTUnMCveE8u3DcHs3jhPxV0LWOU7utyvSpaeBAjvMGHCxiNxj09Mv4ojYoYZAP3fjKHBK4XZPfa6zN5CUQBbkemj7WkDihsNx7UuvNpWpNqv4uWqM1bT3Qp6nGsEUpCLtu0aa7IacaG9FDKpIl4YlcBZHmF5S0HlIzuCg5d2A9swop4whqSPY1vQjOVIys6p6x5U0BZryQIKqfyYomTNTAqHH2DRXBBQsvgYbUGy3RWCxBpqD3lBmk4t4guNKZeXsljF9kLmESRnchzu0EDV1mv8zfowCKF69CoXrTuaFO19xDgx5UhgDpRGNLO0mUxBXIiq3ktTnhwM4TublsRFQQ7oNfZhd3hrgEUxQn8VOxGeNFqTbRMjwJSJuE4mnRjv8IS5gtileIIpmTsZZGPE0wJdhyw2ifefAjIntPF7e829q";
     private $defaultUserId='319';
     private $dollarRatio=66;
 
@@ -14,10 +14,7 @@ class Sync extends Catalog {
             SELECT
                     product_code,
                     ru product_name,
-                    ROUND(sell
-                    *IF(discount IS NOT NULL,discount,1)
-                    *IF(curr_code='USD',$this->dollarRatio,1),2)
-                        product_price,
+                    GET_PRICE(product_code,'$this->defaultUserId',$this->dollarRatio) product_price,
                     product_quantity,
                     product_volume,
                     product_weight,
@@ -31,8 +28,6 @@ class Sync extends Catalog {
                     price_list USING(product_code)
                         JOIN
                     stock_tree st ON se.parent_id=st.branch_id
-                        LEFT JOIN
-                    companies_discounts cd ON st.top_id = cd.branch_id
                 WHERE company_id='$this->defaultUserId'
                 ORDER BY fetch_count DESC
                 LIMIT $limit OFFSET $offset";
