230e989f52ad45f71eae4af1a21fbeb786997812
Baycik <Baycik@192.168.3.2>
2017-05-03 21:53:33 +0300

    working on plugin compability

---
+++ application/plugins/StockBuyManager/models/StockBuyManager.php
@@ -0,0 +1,13 @@
+<?php
+/* Group Name: Склад
+ * User Level: 2
+ * Plugin Name: Менеджер закупок
+ * Plugin URI: http://isellsoft.com
+ * Version: 0.1
+ * Description: Tool for managing income buyes
+ * Author: baycik 2011
+ * Author URI: http://isellsoft.com
+ */
+class StockBuyManager extends Catalog{
+    
+}
