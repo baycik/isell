7b2e3c012eda5e502c0f17d3c2b866b31ce5476f
baycik <baycik@DESKTOP-DKE3FOL>
2017-06-07 15:03:17 +0300

    working on buy manager

--- application/plugins/StockBuyManager/models/install.sql
+++ application/plugins/StockBuyManager/models/install.sql
@@ -39,3 +39,11 @@ CREATE TABLE `supplier_list` (
   `supplier_sell_gain` double DEFAULT '0',
   PRIMARY KEY (`supplier_company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `isell_db`.`supply_order_list` (
+  `entry_id` INT NOT NULL AUTO_INCREMENT,
+  `product_code` VARCHAR(45) NULL,
+  `product_quantity` INT NULL,
+  `product_comment` VARCHAR(45) NULL,
+  PRIMARY KEY (`entry_id`));
