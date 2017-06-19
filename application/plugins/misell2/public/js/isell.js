c8b695fe37e3667f998538e1e5dea44ade4ec1b7
baycik <baycik@DESKTOP-DKE3FOL>
2017-06-19 17:39:21 +0300

    working on misell

--- application/plugins/misell2/public/js/isell.js
+++ application/plugins/misell2/public/js/isell.js
@@ -1,17 +1,11 @@
-// Initialize your app
-var myApp = new Framework7();
-
-// Export selectors engine
+var misell = new Framework7();
 var $$ = Dom7;
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
+var mainView = misell.addView('.view-main', {
     dynamicNavbar: true
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
+misell.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
     $$('.create-page').on('click', function () {
         createContentPage();
