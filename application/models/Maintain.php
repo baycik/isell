b9781be36e295c9fe5919b475099ef9a746c5f52
Baycik <Baycik@192.168.3.2>
2018-08-29 20:01:39 +0300

    working on automated backup upload

--- application/models/Maintain.php
+++ application/models/Maintain.php
@@ -1,166 +1,194 @@
 <?php
 
 class Maintain extends CI_Model {
-    
-    private $dirDbBackup="/ISELL_DBBACKUP/";
+
+    private $dirDbBackup = "/ISELL_DBBACKUP/";
     private $dirWork;
-    
-    function __construct(){
+
+    function __construct() {
 	$this->dirWork = realpath('.');
     }
-    
-    public $getCurrentVersionStamp=[];
-    public function getCurrentVersionStamp(){
-	if( file_exists($this->dirWork.'/.git') ){
-	    return ['stamp'=>date ("Y-m-d\TH:i:s\Z", time()),'branch'=>$this->getGitBranch()];
+
+    public $getCurrentVersionStamp = [];
+
+    public function getCurrentVersionStamp() {
+	if (file_exists($this->dirWork . '/.git')) {
+	    return ['stamp' => date("Y-m-d\TH:i:s\Z", time()), 'branch' => $this->getGitBranch()];
 	}
-	return ['stamp'=>date ("Y-m-d\TH:i:s\Z", filemtime($this->dirWork)),'branch'=>$this->getGitBranch()];
+	return ['stamp' => date("Y-m-d\TH:i:s\Z", filemtime($this->dirWork)), 'branch' => $this->getGitBranch()];
     }
-    
-    private function getGitBranch(){
-	$matches=[];
-        preg_match("/\/(\w+).zip/", BAY_UPDATE_URL, $matches);
+
+    private function getGitBranch() {
+	$matches = [];
+	preg_match("/\/(\w+).zip/", BAY_UPDATE_URL, $matches);
 	return $matches[1];
     }
-    
-    public $updateInstall=[];
-    public function updateInstall(){
+
+    public $updateInstall = [];
+
+    public function updateInstall() {
 	$this->updateConfigurator();
 	$this->updateDb();
 	return true;
     }
-    
-    private function updateDb(){
-	$result=$this->db->query("SELECT pref_value FROM pref_list WHERE pref_name='db_applied_patches'");
-	$db_applied_patches=$result->row()?$result->row()->pref_value:'';
-	$directory = str_replace("\\", "/", $this->dirWork.'/install/db_update/');
+
+    private function updateDb() {
+	$result = $this->db->query("SELECT pref_value FROM pref_list WHERE pref_name='db_applied_patches'");
+	$db_applied_patches = $result->row() ? $result->row()->pref_value : '';
+	$directory = str_replace("\\", "/", $this->dirWork . '/install/db_update/');
 	$patches = array_diff(scandir($directory), array('..', '.'));
-	foreach($patches as $patch){
-	    $patch_version=  str_replace('.sql', '', $patch);
-	    if( strpos($db_applied_patches,$patch_version)===false ){
-		$this->backupImportExecute($directory.$patch);
-		$db_applied_patches.="|".$patch_version;
+	foreach ($patches as $patch) {
+	    $patch_version = str_replace('.sql', '', $patch);
+	    if (strpos($db_applied_patches, $patch_version) === false) {
+		$this->backupImportExecute($directory . $patch);
+		$db_applied_patches.="|" . $patch_version;
 		$this->db->query("REPLACE pref_list SET pref_name='db_applied_patches', pref_value='$db_applied_patches'");
 	    }
 	}
     }
-    
-    private function updateConfigurator(){
-	$this->xcopy($this->dirWork.'/install/configurator',realpath('../') );
+
+    private function updateConfigurator() {
+	$this->xcopy($this->dirWork . '/install/configurator', realpath('../'));
     }
-    
-    private function xcopy($src,$dst) {
+
+    private function xcopy($src, $dst) {
 	$dir = opendir($src);
 	!file_exists($dst) && mkdir($dst);
-	while( false !== ($file = readdir($dir)) ) {
-	    if( $file=='.' || $file=='..' ){
+	while (false !== ($file = readdir($dir))) {
+	    if ($file == '.' || $file == '..') {
 		continue;
 	    }
-	    if ( is_dir($src.'/'.$file) ) {
-		$this->xcopy( $src.'/'.$file, $dst.'/'.$file );
+	    if (is_dir($src . '/' . $file)) {
+		$this->xcopy($src . '/' . $file, $dst . '/' . $file);
 	    } else {
-		copy( $src.'/'.$file, $dst.'/'.$file );
+		copy($src . '/' . $file, $dst . '/' . $file);
 	    }
 	}
 	closedir($dir);
     }
-    
-    private function setupConf(){
-	$conf_file=  $this->dirWork."/conf".rand(1,1000);
-	$conf='[client]
-	    user="'.BAY_DB_USER.'"
-	    password="'.BAY_DB_PASS.'"
+
+    private function setupConf() {
+	$conf_file = $this->dirWork . "/conf" . rand(1, 1000);
+	$conf = '[client]
+	    user="' . BAY_DB_USER . '"
+	    password="' . BAY_DB_PASS . '"
 	    default-character-set=utf8
 	    ';
 	file_put_contents($conf_file, $conf);
 	return $conf_file;
     }
-    
-    public function backupImportExecute( $file ){
-	$output=[];
-	$conf_file=$this->setupConf();
-        $path_to_mysql=$this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
-	exec("$path_to_mysql/bin/mysql --defaults-file=$conf_file ".BAY_DB_NAME." <".$file." 2>&1",$output);
+
+    public function backupImportExecute($file) {
+	$output = [];
+	$conf_file = $this->setupConf();
+	$path_to_mysql = $this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
+	exec("$path_to_mysql/bin/mysql --defaults-file=$conf_file " . BAY_DB_NAME . " <" . $file . " 2>&1", $output);
 	unlink($conf_file);
-	if( count($output) ){
-	    file_put_contents($this->dirDbBackup.date('Y-m-d_H-i-s').'-IMPORT.log', implode( "\n", $output ));
+	if (count($output)) {
+	    file_put_contents($this->dirDbBackup . date('Y-m-d_H-i-s') . '-IMPORT.log', implode("\n", $output));
 	    return false;
 	}
 	return true;
     }
-    
-    public $backupImport=['filename'=>'string'];
-    public function backupImport($file){
+
+    public $backupImport = ['filename' => 'string'];
+
+    public function backupImport($file) {
 	$this->Hub->set_level(4);
-	if( file_exists($this->dirDbBackup.$file) ){
-	    return $this->backupImportExecute($this->dirDbBackup.$file);
+	if (file_exists($this->dirDbBackup . $file)) {
+	    return $this->backupImportExecute($this->dirDbBackup . $file);
 	}
-        return false;
+	return false;
     }
-    
-    public $backupDump=[];
-    public function backupDump(){
-        $this->Hub->set_level(4);
-        $path_to_mysql=$this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
-	if( !file_exists ($this->dirDbBackup) ){
+
+    public $backupDump = [];
+
+    public function backupDump() {
+	$this->Hub->set_level(4);
+	$path_to_mysql = $this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
+	if (!file_exists($this->dirDbBackup)) {
 	    mkdir($this->dirDbBackup);
 	}
-        $output=[];
-        $filename=$this->dirDbBackup.date('Ymd_His')."_".BAY_DB_NAME.'_BACKUP.sql';
-        exec("$path_to_mysql/bin/mysqldump --user=".BAY_DB_USER." --password=".BAY_DB_PASS."  --default-character-set=utf8 --single-transaction=TRUE --routines --events  ".BAY_DB_NAME." >".$filename,$output);
-        if( count($output) ){
-            file_put_contents($filename.'.log', implode( "\n", $output ));
-            return false;
-        }
-        return true;
+	$output = [];
+	$filename = $this->dirDbBackup . date('Ymd_His') . "_" . BAY_DB_NAME . '_BACKUP.sql';
+	exec("$path_to_mysql/bin/mysqldump --user=" . BAY_DB_USER . " --password=" . BAY_DB_PASS . "  --default-character-set=utf8 --single-transaction=TRUE --routines --events  " . BAY_DB_NAME . " >" . $filename, $output);
+	if (count($output)) {
+	    file_put_contents($filename . '.log', implode("\n", $output));
+	    return false;
+	}
+
+	//
+	//$this->backupDumpZip($filename);
+	//$this->backupDumpFtpUpload("$filename");
+	return true;
     }
-    
-    public $backupList=[];
-    public function backupList(){
+
+    private function backupDumpZip($filename) {
+	$zip = new ZipArchive;
+	if ($zip->open("$filename.zip", ZipArchive::CREATE) === TRUE) {
+	    $zip->addFile($filename,'backup.sql');
+	    $zip->close();
+	    unlink($filename);
+	    return true;
+	}
+	return false;
+    }
+    private function backupDumpFtpUpload($filename){
+	copy($filename,"ftp:///dbbackup.sql.zip");
+    }
+
+    public $backupList = [];
+
+    public function backupList() {
 	$this->Hub->set_level(4);
 	$files = array_diff(scandir($this->dirDbBackup), array('.', '..'));
 	arsort($files);
-	return array_values ($files);
+	return array_values($files);
     }
-    
-    public $backupListNamed=[];
-    public function backupListNamed(){
+
+    public $backupListNamed = [];
+
+    public function backupListNamed() {
 	$this->Hub->set_level(4);
-	$named=[];
-	$list=$this->backupList();
-	foreach($list as $file){
-	    $named[]=['file'=>$file];
+	$named = [];
+	$list = $this->backupList();
+	foreach ($list as $file) {
+	    $named[] = ['file' => $file];
 	}
 	return $named;
     }
-    
-    public $backupDown=['string'];
-    public function backupDown( $file ){
+
+    public $backupDown = ['string'];
+
+    public function backupDown($file) {
 	$this->Hub->set_level(4);
-	if( file_exists ($this->dirDbBackup.$file) ){
+	if (file_exists($this->dirDbBackup . $file)) {
 	    header('Content-type: application/force-download');
-	    header('Content-Disposition: attachment; filename="'.$file.'"');
-	    echo file_get_contents($this->dirDbBackup.$file);
+	    header('Content-Disposition: attachment; filename="' . $file . '"');
+	    echo file_get_contents($this->dirDbBackup . $file);
 	} else {
-	    show_error('X-isell-error: File not found!'.$this->dirDbBackup.$file, 404);
+	    show_error('X-isell-error: File not found!' . $this->dirDbBackup . $file, 404);
 	}
     }
-    
-    public $backupUp=[];
-    public function backupUp(){
-	if( !file_exists ($this->dirDbBackup) ){
+
+    public $backupUp = [];
+
+    public function backupUp() {
+	if (!file_exists($this->dirDbBackup)) {
 	    mkdir($this->dirDbBackup);
 	}
-	if( $_FILES['upload_file'] && !$_FILES['upload_file']['error'] ){
-	    return 'uploaded'.move_uploaded_file( $_FILES['upload_file']["tmp_name"] , $this->dirDbBackup.$_FILES['upload_file']['name'] );
+	if ($_FILES['upload_file'] && !$_FILES['upload_file']['error']) {
+	    return 'uploaded' . move_uploaded_file($_FILES['upload_file']["tmp_name"], $this->dirDbBackup . $_FILES['upload_file']['name']);
 	}
-        return 'error'.$_FILES['upload_file']['error'];
+	return 'error' . $_FILES['upload_file']['error'];
     }
-    
-    public $backupDelete=[];
-    public function backupDelete(){
+
+    public $backupDelete = [];
+
+    public function backupDelete() {
 	$this->Hub->set_level(4);
-        $file=$this->input->post('filename');
-	return unlink($this->dirDbBackup.$file);
+	$file = $this->input->post('filename');
+	return unlink($this->dirDbBackup . $file);
     }
+
 }
