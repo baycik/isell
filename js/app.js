1cf09825e148fb1cc70ee646438b1367154d8a12
adamHider <adamHider@adamHider>
2018-10-30 15:07:08 +0300

    checkout release Beta

--- js/app.js
+++ js/app.js
@@ -628,15 +628,33 @@ Mark.pipes.format = function (str) {
 	    };
 	    
 	    App.module={
-		init:function(){
-		    $(window).bind( 'hashchange', function(e) {
-			App.module.load(location.hash.substring(1));
+                init: function () {
+		    $(window).bind('hashchange', function (e) {
+			App.module.hashchanged(location.hash.substring(1));
 		    });
 		    if( !location.hash ){
 			location.hash="#"+App.user.props.module_list[0].name;
 		    }
-		    App.module.load(location.hash.substring(1));
+		    App.module.hashchanged(location.hash.substring(1));
 		},
+                hashchanged:function(hash){
+		    var url_chunks = hash.split("#");
+ 		    App.url_query = url_chunks[1];
+		    App.module.parseState(App.url_query);
+                    this.load(url_chunks[0]);
+                    App.Topic('hashChange').publish(App.state);
+                },
+                parseState: function (text) {
+                    var newstate = {};
+                    if (text) {
+                        var pairs = text.split('&');
+                        for (var i in pairs) {
+                            var keyval = pairs[i].split('=');
+                            newstate[keyval[0]] = keyval[1];
+                        }
+                    }
+                    App.state = newstate;
+                },
 		load:function(name){
 		    if( this.current === name ){
 			return false;
