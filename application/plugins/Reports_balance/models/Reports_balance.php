83aeabcb1fec5a228df93d3620057e621ab37472
admin <admin@BaycikNote>
2018-12-20 15:56:41 +0300

    working on balance report

--- application/plugins/Reports_balance/models/Reports_balance.php
+++ application/plugins/Reports_balance/models/Reports_balance.php
@@ -1,6 +1,6 @@
 <?php
 /* Group Name: Статистика
- * User Level: 1
+ * User Level: 4
  * Plugin Name: Баланс
  * Plugin URI: 
  * Version: 0.1
@@ -12,97 +12,88 @@
 class Reports_balance extends Catalog{
     private $all_active;
     public function __construct() {
-	$this->all_active=$this->request('all_active','bool');
-        $this->idate=$this->dmy2iso( $this->request('idate','\d\d.\d\d.\d\d\d\d') );
-        $this->fdate=$this->dmy2iso( $this->request('fdate','\d\d.\d\d.\d\d\d\d') );
 	parent::__construct();
+	$this->all_active=$this->request('all_active','bool');
+        $this->idate= $this->request('idate','\d\d\d\d.\d\d.\d\d');
+        $this->fdate=$this->request('fdate','\d\d\d\d.\d\d.\d\d');
+        $this->accounts=[
+            'active'=>[
+                '301'
+            ],
+            'passive'=>[
+                '63'
+            ]
+        ];
     }
     private function getAssignedPathWhere(){
         $assigned_path=$this->Hub->svar('user_assigned_path');
         return $assigned_path?"AND (path LIKE '".str_replace(',',"%' OR path LIKE '",$assigned_path.'')."%')":"";    
     }
-    public function viewGet(){
+    private function getAccountLeftover($acc_code,$fdate_list){
 	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Hub->acomp('company_id');
-        $date_filter='';
-        if( $this->fdate ){
-            $date_filter="AND cstamp<'$this->fdate 23:59:59' ";
+        $leftovers=[];
+        foreach($fdate_list as $i=>$fdate){
+            $leftovers[]="
+                (SELECT 
+                        ROUND(SUM(amount) / 1000, 0)
+                    FROM
+                        acc_trans
+                    WHERE
+                        cstamp<'$fdate'
+                        AND acc_debit_code LIKE CONCAT(acc_code,'%')
+                        $active_filter) 
+                -
+                
+                (SELECT 
+                        ROUND(SUM(amount) / 1000, 0)
+                    FROM
+                        acc_trans
+                    WHERE
+                        cstamp<'$fdate'
+                        AND acc_credit_code LIKE CONCAT(acc_code,'%')
+                        $active_filter) leftover$i";
         }
-        $user_level=$this->Hub->svar('user_level');
-        $path_filter=$this->getAssignedPathWhere();
-	$having =$this->getDirectionFilter();
-        $having.=$this->filter_value?"AND (".$this->or_like($this->filter_by,$this->filter_value).")":"";
-	$sql="
-	    SELECT
-		label,
-		REPLACE(path,'/','/ ') path,
-                deferment,
-		phone,
-                buy,
-                sell,
-                ROUND(IF(sell > allow, sell - allow, 0)) AS exp,
-                FLOOR(expday / 30.417) m,
-                ROUND(expday - FLOOR(expday / 30.417) * 30.417) d
-	    FROM
-		(SELECT 
-		    path,
-		    label,
-		    deferment,
-		    ROUND(SUM(IF(acc_debit_code=361,amount,IF(acc_credit_code=361,-amount,0))),2) sell,
-		    ROUND(SUM(IF(acc_debit_code=631,-amount,IF(acc_credit_code=631,amount,0))),2) buy,
-		    ROUND(SUM(
-		    IF(
-		    DATEDIFF(NOW(),acc_trans.cstamp)<=deferment AND (trans_status=1 OR trans_status=2),IF(acc_debit_code=361,amount,0),0)
-		    ),2) allow,
-		    MAX(IF(DATEDIFF(NOW(),acc_trans.cstamp)>deferment AND (trans_status=1 OR trans_status=2),DATEDIFF(NOW(),acc_trans.cstamp),0)) AS expday,
-		    CONCAT(company_mobile,' ',company_phone) phone
-                FROM
-		    companies_list
-			LEFT JOIN 
-		    acc_trans ON company_id=passive_company_id
-			LEFT JOIN
-		    companies_tree USING(branch_id)
-		WHERE 
-		    level<='$user_level'
-                    $date_filter
-		    $active_filter
-		    $path_filter
-                GROUP BY companies_list.company_id
-		
-		ORDER BY expday DESC) expired
-		$having";
-	
-	//echo "<pre>$sql";
-	//die();
-	
-	$rows=$this->get_list($sql);
-
-	$total_our_debt=0;
-        $total_their_debt=0;
-        $total_their_exp=0;
-        foreach( $rows as $row ){
-            $total_our_debt+=$row->buy;
-            $total_their_debt+=$row->sell;
-            $total_their_exp+=$row->exp;
-	    $row->buy=$row->buy!=0?$row->buy:'';
-	    $row->sell=$row->sell!=0?$row->sell:'';
-	    $row->exp=$row->exp!=0?$row->exp:'';
-	    $row->m=$row->m!=0?$row->m:'';
-	    $row->d=$row->d!=0?$row->d:'';
-	    $row->deferment=$row->deferment!=0?$row->deferment:'';
+        $leftover_queries= implode(',', $leftovers);
+        $sql="SELECT 
+                acc_code,label acc_name,$leftover_queries
+            FROM
+                acc_tree
+            WHERE
+                acc_code LIKE '{$acc_code}_'";
+        return $this->get_list($sql);
+    }
+    private function iso2dmy( $iso ){
+	$chunks=  explode('-', $iso);
+	return "$chunks[2].$chunks[1].$chunks[0]";
+    }
+    public function viewGet(){
+        
+        $fdate_list=[$this->fdate];
+        
+        $v=[
+            'active'=>[],
+            'passive'=>[]
+        ];
+        foreach( $this->accounts['active'] as $acc_code ){
+            $v['active']= array_merge($v['active'],$this->getAccountLeftover($acc_code, $fdate_list));
         }
-	return [
-	    'total_our_debt'=>$total_our_debt,
-	    'total_their_debt'=>$total_their_debt,
-	    'total_their_exp'=>$total_their_exp,
-	    'rows'=>count($rows)?$rows:[[]],
-	    'input'=>[
-		'all_active'=>$this->all_active,
-                'fdate'=>$this->iso2dmy($this->fdate),
-		'our_debts'=>$this->our_debts,
-		'their_debts'=>$this->their_debts,
-		'filter_by'=>$this->filter_by,
-		'filter_value'=>$this->filter_value
-	    ]
-	];
+        foreach( $this->accounts['passive'] as $acc_code ){
+            $v['passive']= array_merge($v['passive'],$this->getAccountLeftover($acc_code, $fdate_list));
+        }
+        
+        foreach( $fdate_list as $i=>$fdate ){
+            $v['active_total_'.$i]=
+        }
+        
+        
+        
+        $v['input']=[
+            'all_active'=>$this->all_active,
+            'fdate0'=>$this->iso2dmy($this->fdate)
+        ];
+        
+        print_r($v);
+        
+	return $v;
     }
 }
