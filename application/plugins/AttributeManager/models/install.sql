1035920ff659c5d7d443ac5734429226ac2f7923
admin <admin@BaycikNote>
2019-01-31 13:45:12 +0300

    bugfix

--- application/plugins/AttributeManager/models/install.sql
+++ application/plugins/AttributeManager/models/install.sql
@@ -1,7 +1,14 @@
 /**
- * Author:  Baycik
- * Created: May 4, 2017
+ * Author:  AdamHider
  */
+CREATE TABLE `attribute_list` (
+  `attribute_id` int(11) NOT NULL AUTO_INCREMENT,
+  `attribute_name` varchar(255) DEFAULT NULL,
+  `attribute_unit` varchar(225) DEFAULT NULL,
+  PRIMARY KEY (`attribute_id`),
+  UNIQUE KEY `attribute_id_UNIQUE` (`attribute_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `attribute_values` (
   `attribute_id` int(11) NOT NULL,
@@ -13,12 +20,4 @@ CREATE TABLE `attribute_values` (
   CONSTRAINT `fk_ayder_prod_list` FOREIGN KEY (`product_id`) REFERENCES `prod_list` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `attribute_list` (
-  `attribute_id` int(11) NOT NULL AUTO_INCREMENT,
-  `attribute_name` varchar(255) DEFAULT NULL,
-  `attribute_unit` varchar(225) DEFAULT NULL,
-  PRIMARY KEY (`attribute_id`),
-  UNIQUE KEY `attribute_id_UNIQUE` (`attribute_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
