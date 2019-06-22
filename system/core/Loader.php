9221fe4977c9cb0c783596cbb9ce1c3cbf447284
admin <admin@BaycikNote>
2019-06-22 11:39:38 +0300

    model loader fix

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -293,6 +293,7 @@ class CI_Loader {
 		$model = ucfirst($model);
 		if ( ! class_exists($model))
 		{
+                    echo $model;
 			foreach ($this->_ci_model_paths as $mod_path)
 			{
 				if ( ! file_exists($mod_path.'models/'.$path.$model.'.php'))
