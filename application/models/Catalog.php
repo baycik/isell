a6d0c2c37ee2a650500538e90fc009dfc6ed3f4a
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-02-11 17:34:44 +0300

    working on moedelosync

--- application/models/Catalog.php
+++ application/models/Catalog.php
@@ -158,7 +158,7 @@ class Catalog extends CI_Model {
 
     public function log($message) {
         $class = get_class($this);
-        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI] User:".$this->svar('user_login');
+        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI] User:".$this->Hub->svar('user_login');
         $this->create('log_list', ['message' => $message, 'url' => $url, 'log_class' => $class]);
     }
 
