a22345e35f3b7806bf6cd16376b86332d73b30b9
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-03-28 14:47:39 +0300

    working on MoedeloSync

--- application/plugins/MoedeloSync/models/MoedeloSync.php
+++ application/plugins/MoedeloSync/models/MoedeloSync.php
@@ -44,12 +44,12 @@ class MoedeloSync extends Catalog {
 
     //'MoedeloSyncStocks/replicate/1 years/','MoedeloSyncStocks/checkout/1 years/',
     private $joblist=[
-            //'MoedeloSyncProduct/localCheckout/9 minutes/9 minutes',
-            //'MoedeloSyncProduct/remoteCheckout/9 seconds/9 hours',
-            //'MoedeloSyncProduct/replicate/10 minutes/',
+            'MoedeloSyncProduct/localCheckout/9 minutes/60 minutes',
+            'MoedeloSyncProduct/remoteCheckout/9 minutes/1 days',
+            'MoedeloSyncProduct/replicate/10 minutes/',
         
-            /*'MoedeloSyncCompanies/localCheckout/9 minutes/9 minutes',
-            'MoedeloSyncCompanies/remoteCheckout/1 minutes/1 minutes',
+            'MoedeloSyncCompanies/localCheckout/9 minutes/60 minutes',
+            'MoedeloSyncCompanies/remoteCheckout/9 minutes/1 days',
             'MoedeloSyncCompanies/replicate/10 minutes/',
             
             'MoedeloSyncBillSell/localCheckout/9 minutes/60 minutes',
@@ -64,9 +64,11 @@ class MoedeloSync extends Catalog {
             'MoedeloSyncInvoiceSell/remoteCheckout/9 minutes/1 days',
             'MoedeloSyncInvoiceSell/replicate/10 minutes/',
             
-            'MoedeloSyncUPDSell/localCheckout/9 seconds/60 minutes',
-            //'MoedeloSyncUPDSell/remoteCheckout/9 seconds/1 days',
-            'MoedeloSyncUPDSell/replicate/10 seconds/'*/
+            'MoedeloSyncUPDSell/localCheckout/9 minutes/60 minutes',
+            'MoedeloSyncUPDSell/replicate/10 minutes/',
+        
+            'MoedeloSyncUPDBuy/localCheckout/9 minutes/60 minutes',
+            'MoedeloSyncUPDBuy/replicate/10 minutes/',
         
             'MoedeloSyncActSell/localCheckout/9 minutes/60 minutes',
             'MoedeloSyncActSell/remoteCheckout/9 minutes/1 days',
@@ -82,8 +84,7 @@ class MoedeloSync extends Catalog {
             
             'MoedeloSyncInvoiceBuy/localCheckout/9 minutes/60 minutes',
             'MoedeloSyncInvoiceBuy/remoteCheckout/9 minutes/1 days',
-            'MoedeloSyncInvoiceBuy/replicate/10 minutes/',
-            
+            'MoedeloSyncInvoiceBuy/replicate/10 minutes/'
         ];
     
     public function tick( $iterations_left ){
