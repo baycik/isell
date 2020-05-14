85b70f738ff175c3143a01ac0b8bc48de1588d89
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-05-14 10:15:52 +0300

    working on balance report

--- application/plugins/Reports_balance/models/Reports_balance.php
+++ application/plugins/Reports_balance/models/Reports_balance.php
@@ -3,9 +3,9 @@
  * User Level: 4
  * Plugin Name: Баланс
  * Plugin URI: 
- * Version: 0.1
+ * Version: 1
  * Description: 
- * Author: baycik 2018
+ * Author: baycik 2020
  * Author URI: 
  * Trigger before: Reports_balance
  */
@@ -14,14 +14,13 @@ class Reports_balance extends Catalog{
     public function __construct() {
 	parent::__construct();
 	$this->all_active=$this->request('all_active','bool');
-        $this->idate= $this->request('idate','\d\d\d\d.\d\d.\d\d');
         $this->fdate=$this->request('fdate','\d\d\d\d.\d\d.\d\d');
         $this->accounts=[
             'active'=>[
-                '301'
+                '3'
             ],
             'passive'=>[
-                '63'
+                '6'
             ]
         ];
     }
@@ -29,12 +28,13 @@ class Reports_balance extends Catalog{
         $assigned_path=$this->Hub->svar('user_assigned_path');
         return $assigned_path?"AND (path LIKE '".str_replace(',',"%' OR path LIKE '",$assigned_path.'')."%')":"";    
     }
-    private function getAccountLeftover($acc_code,$fdate_list){
+    private function getAccountLeftover($acc_code){
+        $fdate_list=[$this->fdate];
 	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Hub->acomp('company_id');
         $leftovers=[];
         foreach($fdate_list as $i=>$fdate){
             $leftovers[]="
-                (SELECT 
+                COALESCE((SELECT 
                         ROUND(SUM(amount) / 1000, 0)
                     FROM
                         acc_trans
@@ -51,34 +51,81 @@ class Reports_balance extends Catalog{
                     WHERE
                         cstamp<'$fdate'
                         AND acc_credit_code LIKE CONCAT(acc_code,'%')
-                        $active_filter) leftover$i";
+                        $active_filter),'') leftover$i";
         }
         $leftover_queries= implode(',', $leftovers);
-        $sql="SELECT 
+        $lvl3_sql="SELECT 
                 acc_code,label acc_name,$leftover_queries
             FROM
                 acc_tree
             WHERE
-                acc_code LIKE '{$acc_code}_'";
-        return $this->get_list($sql);
+                acc_code LIKE '{$acc_code}__'
+            HAVING leftover0
+            ORDER BY acc_code
+                ";
+        return $this->get_list($lvl3_sql);;
     }
+    
+    
+    private function getAccountTree($leftovers){
+        $subtotals=[];
+        foreach($leftovers as $leftover){
+            $acc_code=$leftover['acc_code'];
+            for($i=0;$i<count($acc_code);$i++){
+                $group_code=substr($acc_code,0,$i);
+                $subtotals[$group_code]=$subtotals[$group_code]??0+$leftover['leftover0'];
+            }
+        }
+        
+        
+        
+        
+        
+        
+        $codes  = array_column($group_rows, 'acc_code');
+        array_multisort($codes, SORT_STRING, $group_rows);
+        
+        
+        
+        
+    }
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     private function iso2dmy( $iso ){
 	$chunks=  explode('-', $iso);
 	return "$chunks[2].$chunks[1].$chunks[0]";
     }
     public function viewGet(){
-        
-        $fdate_list=[$this->fdate];
+        $this->Hub->set_level(4);
         
         $v=[
             'active'=>[],
             'passive'=>[]
         ];
         foreach( $this->accounts['active'] as $acc_code ){
-            $v['active']= array_merge($v['active'],$this->getAccountLeftover($acc_code, $fdate_list));
+            $v['active']= array_merge($v['active'],$this->getAccountLeftover($acc_code));
         }
         foreach( $this->accounts['passive'] as $acc_code ){
-            $v['passive']= array_merge($v['passive'],$this->getAccountLeftover($acc_code, $fdate_list));
+            //$v['passive']= array_merge($v['passive'],$this->getAccountLeftover($acc_code, $fdate_list));
         }
         
         foreach( $fdate_list as $i=>$fdate ){
@@ -92,7 +139,7 @@ class Reports_balance extends Catalog{
             'fdate0'=>$this->iso2dmy($this->fdate)
         ];
         
-        print_r($v);
+        //header("Content-type:text/plain");print_r($v);
         
 	return $v;
     }
