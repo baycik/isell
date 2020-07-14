07e3ce3b9688c199f5be208f32c09359254b882e
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-07-14 11:37:03 +0300

    working on moedelosync

--- application/plugins/MoedeloSync/models/MoedeloSyncUPDBuy.php
+++ application/plugins/MoedeloSync/models/MoedeloSyncUPDBuy.php
@@ -119,6 +119,7 @@ class MoedeloSyncUPDBuy extends MoedeloSyncUPDSell{
                     IF({$this->doc_config->doc_type}=1 OR {$this->doc_config->doc_type}=2,1,2) Type,
                     IF({$document->vat_rate},5,0) NdsType,
                     ROUND(invoice_price,2) Price,
+                    ROUND(invoice_price*product_quantity,2) SumWithoutNds,
                     ROUND(invoice_price*product_quantity*(1+{$document->vat_rate}/100),2) SumWithNds,
                     prod_pse.remote_id StockProductId
                 FROM
