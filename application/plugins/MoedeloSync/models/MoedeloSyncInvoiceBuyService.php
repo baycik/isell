b7e5633c2c6fac3847b2f290186f689bf78415e3
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-07-14 14:40:17 +0300

    working on moedelosync

--- application/plugins/MoedeloSync/models/MoedeloSyncInvoiceBuyService.php
+++ application/plugins/MoedeloSync/models/MoedeloSyncInvoiceBuyService.php
@@ -6,7 +6,7 @@ class MoedeloSyncInvoiceBuyService extends MoedeloSyncInvoiceBuy{
         $this->doc_config=(object) [
             'remote_function'=>'accounting/api/v1/purchases/invoice/common',
             'local_view_type_id'=>140,//invoice
-            'sync_destination'=>'moedelo_doc_invoice_buy',
+            'sync_destination'=>'moedelo_doc_invoice_buy_service',
             'doc_type'=>4
         ];
     }
