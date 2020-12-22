102a66440ef27b31bdd2878404765eee31d92a42
bayci <bayci@DESKTOP-5FND87U.Dlink>
2020-12-22 16:00:13 +0300

    working on mailing manager

--- application/plugins/MailingManager/handlers/SmsHandler.php
+++ application/plugins/MailingManager/handlers/SmsHandler.php
@@ -3,7 +3,7 @@
 class SmsHandler  extends MailingManager{
     public function send( $number, $message ){
         $Utils = $this->MailingManager->Hub->load_model('Utils');
-        $body = $message->message_body;
+        $body = strip_tags($message->message_body);
         return $Utils->sendSms($number, $body);
     }
 }
