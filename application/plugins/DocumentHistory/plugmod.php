c07ff198c6c6617560130a3a2f115945d7d3aded
bayci <bayci@DESKTOP-5FND87U.Dlink>
2021-02-09 12:44:51 +0300

    bugfixes

--- application/plugins/DocumentHistory/plugmod.php
+++ application/plugins/DocumentHistory/plugmod.php
@@ -8,8 +8,8 @@ EOT;
 /*
  * Search for string
  */
-$search[]=<<<EOT
-\$this->permited_tables = json_decode(file_get_contents('application/config/permited_tables.json', true));
+$search[]=<<<'EOT'
+$this->permited_tables = json_decode(file_get_contents('application/config/permited_tables.json', true));
 EOT;
 /*
  * Replace found with
@@ -24,6 +24,6 @@ EOT;
 /*
  * Inserts after found
  */
-$after[]=<<<EOT
-        \$this->permited_tables[]=json_decode('{"table_name":"plugin_doc_history_list","table_title":"История изменений документа","level":2,"editable":0}');
+$after[]=<<<'EOT'
+        $this->permited_tables[]=json_decode('{"table_name":"plugin_doc_history_list","table_title":"История изменений документа","level":2,"editable":0}');
 EOT;
