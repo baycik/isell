10de501dfa62b291ac5a990a8d8b8d558737f2a4
bayci <bayci@DESKTOP-5FND87U.Dlink>
2021-02-15 17:24:25 +0300

    bugfix

--- application/plugins/MailingManager/handlers/EmailHandler.php
+++ application/plugins/MailingManager/handlers/EmailHandler.php
@@ -5,6 +5,9 @@ class EmailHandler  extends MailingManager{
         $Utils = $this->MailingManager->Hub->load_model('Utils');
         $email_subject = $message->message_subject;
         $email_body = $message->message_body;
-        return $Utils->sendEmail($to, $email_subject, $email_body);
+        if(filter_var($to, FILTER_VALIDATE_EMAIL)) {
+            return $Utils->sendEmail($to, $email_subject, $email_body);
+        }
+        return true;//not valid mail skip it!
     }
 }
