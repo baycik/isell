bb2b9a422d6c2406663dad4b6215710e896ab12d
bayci <bayci@DESKTOP-5FND87U.Dlink>
2021-04-26 15:49:08 +0300

    drop transaction_id key

---
+++ install/db_update/2021-04-26-1548.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `isell_db`.`acc_check_list` 
+DROP INDEX `unq` ,
+ADD UNIQUE INDEX `unq` (`number` ASC, `transaction_date` ASC, `correspondent_code` ASC, `debit_amount` ASC, `credit_amount` ASC, `active_company_id` ASC, `assignment` ASC);
+;
