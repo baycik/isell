e05265792324d5b0e84edc4082350e7b5d012884
bayci <bayci@DESKTOP-5FND87U.Dlink>
2022-02-09 10:06:25 +0300

    payment bill personal_interest

--- application/views/rpt/ru/doc/schet-oplata.xlsx.php
+++ application/views/rpt/ru/doc/schet-oplata.xlsx.php
@@ -1,4 +1,14 @@
 <?php
+    if( $this->view->doc_view->extra->personal_interest??0 ){
+        $interest=$this->view->doc_view->extra->personal_interest/100+1;
+        foreach($this->view->rows as $row){
+            $row->product_price_total=round($row->product_price_total*$interest);
+            $row->product_sum_total=round($row->product_sum_total*$interest);
+        }
+        $this->view->footer->vatless=round($this->view->footer->vatless*$interest);
+        $this->view->footer->vat=round($this->view->footer->vat*$interest);
+        $this->view->footer->total=round($this->view->footer->total*$interest);
+    }
     $this->view->a->all=  getAll($this->view->a);
     $this->view->doc_view->total_spell=  num2str($this->view->footer->total);
     $this->view->doc_view->vat_spell=  num2str($this->view->footer->vat);
---
+++ install/db_update/2022-02-08-1225_1.sql
@@ -0,0 +1,1 @@
+UPDATE `document_view_types` SET `view_efield_labels` = '{\"personal_interest\":\"Личный интерес%\"}' WHERE (`view_type_id` = '136');
