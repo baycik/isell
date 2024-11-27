251fa6779a5416c31578c87f03e5e1fa44d62ad1
Bayci <baycikplus@gmail.com>
2024-11-27 13:31:22 +0300

    working on effectiveness report

--- application/plugins/Reports_summary_sell_stock/Reports_summary_sell_stock.php
+++ application/plugins/Reports_summary_sell_stock/Reports_summary_sell_stock.php
@@ -9,115 +9,121 @@
  * Author URI: 
  * Trigger before: Reports_summary_sell_profit
  */
-class Reports_summary_sell_stock extends Catalog{
+class Reports_summary_sell_stock extends Catalog
+{
     private $idate;
     private $fdate;
     private $all_active;
-    public function check( &$var, $type=null ){
-        $type= str_replace('?', '', $type);
-	switch( $type ){
-	    case 'raw':
-		break;
-	    case 'int':
-		$var=(int) $var;
-		break;
-	    case 'float':
-	    case 'double':
-		$var=(float) $var;
-		break;
-	    case 'bool':
-		$var=$var?1:0;
-		break;
-	    case 'escape':
-	    case 'string':
-                $var=  addslashes( $var );
+    public function check(&$var, $type = null)
+    {
+        $type = str_replace('?', '', $type);
+        switch ($type) {
+            case 'raw':
                 break;
-	    case 'json':
-	    case 'array':
-	    case '?array':
-                if( is_array($var) ){
-                    break;//native post array
+            case 'int':
+                $var = (int) $var;
+                break;
+            case 'float':
+            case 'double':
+                $var = (float) $var;
+                break;
+            case 'bool':
+                $var = $var ? 1 : 0;
+                break;
+            case 'escape':
+            case 'string':
+                $var =  addslashes($var);
+                break;
+            case 'json':
+            case 'array':
+            case '?array':
+                if (is_array($var)) {
+                    break; //native post array
                 }
-                $var= trim($var, "\"");
-                $result= json_decode( $var,true );
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    $var=stripslashes($var);
-                    $result= json_decode( $var,true );
+                $var = trim($var, "\"");
+                $result = json_decode($var, true);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    $var = stripslashes($var);
+                    $result = json_decode($var, true);
                 }
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    throw new Exception('JSON error: '.json_last_error_msg(),500);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    throw new Exception('JSON error: ' . json_last_error_msg(), 500);
                 }
-                $var=$result;
+                $var = $result;
                 break;
             case 'object':
             case '?object':
-                $var= trim($var, "\"");
-                $result= json_decode( $var,false ); 
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    $var=stripslashes($var);
-                    $result= json_decode( $var,false );
+                $var = trim($var, "\"");
+                $result = json_decode($var, false);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    $var = stripslashes($var);
+                    $result = json_decode($var, false);
                 }
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    throw new Exception('JSON error: '.json_last_error_msg(),500);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    throw new Exception('JSON error: ' . json_last_error_msg(), 500);
                 }
-                $var=$result;
+                $var = $result;
                 break;
-	    default:
-		if( $type ){
-		    $matches=[];
-		    preg_match('/'.$type.'/u', $var, $matches);
-		    $var=  isset($matches[0])?$matches[0]:null;
-		} else {
-		    $var=  addslashes( $var );
-		}
-	}
+            default:
+                if ($type) {
+                    $matches = [];
+                    preg_match('/' . $type . '/u', $var, $matches);
+                    $var =  isset($matches[0]) ? $matches[0] : null;
+                } else {
+                    $var =  addslashes($var);
+                }
+        }
         return $var;
     }
-    public function request( $name, $type=null, $default=null ){
-	$value=$this->input->get_post($name);
-	if( !is_array($value) && strlen($value)==0 ){
-	    return $default;
-	}
-        return $this->check($value,$type);
-    }
-    public function __construct() {
-	$this->idate=$this->dmy2iso( $this->request('idate','\d\d.\d\d.\d\d\d\d') ).' 00:00:00';
-	$this->fdate=$this->dmy2iso( $this->request('fdate','\d\d.\d\d.\d\d\d\d') ).' 23:59:59';
-	$this->all_active=$this->request('all_active','bool');
-	$this->count_reclamations=$this->request('count_reclamations','bool',0);
-	$this->include_vat=$this->request('include_vat','bool',0);
-	$this->in_alt_currency=$this->request('in_alt_currency','bool',0);
-	$this->show_entries=$this->request('show_entries','bool',0);
-	$this->group_by_filter=$this->request('group_by_filter');
-	$this->group_by=$this->request('group_by','\w+');
-	if( !in_array($this->group_by, ['parent_id','product_code','analyse_type','analyse_brand','analyse_class','product_article']) ){
-	    $this->group_by='parent_id';
-	}
-        
-	$this->group_by2=$this->request('group_by2','\w+');
-        $this->group_by2_comma=$this->group_by2?','.$this->group_by2:'';
-        $this->group_by2_slash=$this->group_by2?",'/',$this->group_by2":'';
-	parent::__construct();
-    }
-    private function dmy2iso( $dmy ){
-	$chunks=  explode('.', $dmy);
-	return "$chunks[2]-$chunks[1]-$chunks[0]";
-    }
-    public function viewGet(){
-	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Hub->acomp('company_id');
-	$reclamation_filter=$this->count_reclamations?'':' AND is_reclamation=0';
-        $having=$this->group_by_filter?"HAVING group_by LIKE '%".str_replace(",", "%' OR group_by LIKE '%", $this->group_by_filter)."%'":"";
-        
-        
-        if( $this->include_vat ){
-            $leftover_calc_mode='selfprice include_vat';
-        } else {
-            $leftover_calc_mode='selfprice';
+    public function request($name, $type = null, $default = null)
+    {
+        $value = $this->input->get_post($name);
+        if (!is_array($value) && strlen($value) == 0) {
+            return $default;
         }
-        
-        
-        $sql_tmp_drop="DROP TEMPORARY TABLE IF EXISTS tmp_summary_sell_stock;";
-        $sell_buy_table="
+        return $this->check($value, $type);
+    }
+    public function __construct()
+    {
+        $this->idate = $this->dmy2iso($this->request('idate', '\d\d.\d\d.\d\d\d\d')) . ' 00:00:00';
+        $this->fdate = $this->dmy2iso($this->request('fdate', '\d\d.\d\d.\d\d\d\d')) . ' 23:59:59';
+        $this->all_active = $this->request('all_active', 'bool');
+        $this->count_reclamations = $this->request('count_reclamations', 'bool', 0);
+        $this->include_vat = $this->request('include_vat', 'bool', 0);
+        $this->in_alt_currency = $this->request('in_alt_currency', 'bool', 0);
+        $this->show_entries = $this->request('show_entries', 'bool', 0);
+        $this->group_by_filter = $this->request('group_by_filter');
+        $this->group_by = $this->request('group_by', '\w+');
+        if (!in_array($this->group_by, ['parent_id', 'product_code', 'analyse_type', 'analyse_brand', 'analyse_class', 'product_article'])) {
+            $this->group_by = 'parent_id';
+        }
+
+        $this->group_by2 = $this->request('group_by2', '\w+');
+        $this->group_by2_comma = $this->group_by2 ? ',' . $this->group_by2 : '';
+        $this->group_by2_slash = $this->group_by2 ? ",'/',$this->group_by2" : '';
+        parent::__construct();
+    }
+    private function dmy2iso($dmy)
+    {
+        $chunks =  explode('.', $dmy);
+        return "$chunks[2]-$chunks[1]-$chunks[0]";
+    }
+    public function viewGet()
+    {
+        $active_filter = $this->all_active ? '' : ' AND active_company_id=' . $this->Hub->acomp('company_id');
+        $reclamation_filter = $this->count_reclamations ? '' : ' AND is_reclamation=0';
+        $having = $this->group_by_filter ? "HAVING group_by LIKE '%" . str_replace(",", "%' OR group_by LIKE '%", $this->group_by_filter) . "%'" : "";
+
+
+        if ($this->include_vat) {
+            $leftover_calc_mode = 'selfprice include_vat';
+        } else {
+            $leftover_calc_mode = 'selfprice';
+        }
+
+
+        $sql_tmp_drop = "DROP TEMPORARY TABLE IF EXISTS tmp_summary_sell_stock;";
+        $sell_buy_table = "
             SELECT
                 product_code,
                 SUM( IF(doc_type=2,product_quantity,-product_quantity) ) stock_qty,
@@ -131,7 +137,7 @@ class Reports_summary_sell_stock extends Catalog{
             WHERE
                 (doc_type=1 OR doc_type=2) AND cstamp<'$this->fdate' AND is_commited=1 AND notcount=0 $active_filter $reclamation_filter
             GROUP BY product_code";
-        $sql_tmp_create="CREATE TEMPORARY TABLE tmp_summary_sell_stock AS(
+        $sql_tmp_create = "CREATE TEMPORARY TABLE tmp_summary_sell_stock AS(
             SELECT 
                 product_code _product_code,
                 ru,
@@ -149,7 +155,7 @@ class Reports_summary_sell_stock extends Catalog{
                 ($sell_buy_table) sellbuy USING(product_code)
             $having
             )";
-        $sql_summary="
+        $sql_summary = "
             SELECT 
                 group_by,
                 SUM(sell_prod_sum) sell_sum,
@@ -164,46 +170,56 @@ class Reports_summary_sell_stock extends Catalog{
                 sell_sum DESC,stock_sum DESC
             ";
         //die($sql);
-        
+
         $this->query($sql_tmp_drop);
         $this->query($sql_tmp_create);
-        
-        
-        $summary_rows=$this->get_list($sql_summary);
-        $total_sell=0;
-        $total_sell_qty=0;
-        $total_stock=0;
-        $total_stock_qty=0;
-        foreach( $summary_rows as $row ){
-            $total_sell+=$row->sell_sum;
-            $total_sell_qty+=$row->sell_sum_qty;
-            $total_stock+=$row->stock_sum;
-            $total_stock_qty+=$row->stock_sum_qty;
+
+
+        $summary_rows = $this->get_list($sql_summary);
+        $total_sell = 0;
+        $total_sell_qty = 0;
+        $total_stock = 0;
+        $total_stock_qty = 0;
+        foreach ($summary_rows as $row) {
+            $total_sell += $row->sell_sum;
+            $total_sell_qty += $row->sell_sum_qty;
+            $total_stock += $row->stock_sum;
+            $total_stock_qty += $row->stock_sum_qty;
         }
-        foreach( $summary_rows as $row ){
-            $row->sell_proc=    $total_sell?round( $row->sell_sum/$total_sell, 4):'';
-            $row->stock_proc=   $total_stock?round( $row->stock_sum/$total_stock, 4):'';
-	    $this->clear_zero($row);
+		
+		$earlier = new DateTime($this->idate);
+		$later = new DateTime($this->fdate);
+
+		$day_span = $later->diff($earlier)->format("%a"); //3
+		$total_stock_sellout = $total_sell_qty>0 ? round( $day_span * $total_stock_qty / $total_sell_qty, 0) : '';
+
+        foreach ($summary_rows as $row) {
+            $row->sell_proc =    $total_sell ? round($row->sell_sum / $total_sell, 4) : '';
+            $row->stock_proc =   $total_stock ? round($row->stock_sum / $total_stock, 4) : '';
+            $row->sellout_days =   $row->sell_sum_qty>0 ? round( $day_span * $row->stock_sum_qty / $row->sell_sum_qty, 0) : '';
+            $this->clear_zero($row);
         }
-        
-        if( $this->show_entries ){
-            $rows=$this->get_list("SELECT * FROM tmp_summary_sell_stock WHERE stock_entry_sum>0 OR sell_prod_sum>0 ORDER BY group_by,_product_code");
+
+        if ($this->show_entries) {
+            $rows = $this->get_list("SELECT * FROM tmp_summary_sell_stock WHERE stock_entry_sum>0 OR sell_prod_sum>0 ORDER BY group_by,_product_code");
         }
-	$view=[
-                'total_sell'=>round($total_sell,2),
-                'total_stock'=>round($total_stock,2),
-                'total_sell_qty'=>round($total_sell_qty,2),
-                'total_stock_qty'=>round($total_stock_qty,2),
-		'summary_rows'=>count($summary_rows)?$summary_rows:[[]],
-		'rows'=>count($rows)?$rows:[[]]
-		];
-	return $view;	
+        $view = [
+            'total_sell' => round($total_sell, 2),
+            'total_stock' => round($total_stock, 2),
+            'total_sell_qty' => round($total_sell_qty, 2),
+            'total_stock_qty' => round($total_stock_qty, 2),
+            'total_stock_sellout' => round($total_stock_sellout, 0),
+            'summary_rows' => count($summary_rows) ? $summary_rows : [[]],
+            'rows' => isset($rows) ? $rows : [[]]
+        ];
+        return $view;
     }
-    private function clear_zero(&$row){
-	foreach($row as &$field){
-	    if(is_numeric($field) && $field==0){
-		$field='';
-	    }
-	}
+    private function clear_zero(&$row)
+    {
+        foreach ($row as &$field) {
+            if (is_numeric($field) && $field == 0) {
+                $field = '';
+            }
+        }
     }
 }
