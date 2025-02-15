0fd5d0d9366c0167d8c2039ca53d291556e4034b
Bayci <baycikplus@gmail.com>
2025-02-15 14:43:30 +0300

    working on balance report

--- application/plugins/Reports_balance/models/Reports_balance.php
+++ application/plugins/Reports_balance/models/Reports_balance.php
@@ -1,7 +1,7 @@
 <?php
 /* Group Name: Статистика
  * User Level: 4
- * Plugin Name: Баланс
+ * Plugin Name: Активы и пассивы
  * Plugin URI: 
  * Version: 1
  * Description: 
@@ -9,97 +9,100 @@
  * Author URI: 
  * Trigger before: Reports_balance
  */
-class Reports_balance extends Catalog{
+class Reports_balance extends Catalog
+{
     private $all_active;
-    public function check( &$var, $type=null ){
-        $type= str_replace('?', '', $type);
-	switch( $type ){
-	    case 'raw':
-		break;
-	    case 'int':
-		$var=(int) $var;
-		break;
-	    case 'float':
-	    case 'double':
-		$var=(float) $var;
-		break;
-	    case 'bool':
-		$var=$var?1:0;
-		break;
-	    case 'escape':
-	    case 'string':
-                $var=  addslashes( $var );
+    public function check(&$var, $type = null)
+    {
+        $type = str_replace('?', '', $type);
+        switch ($type) {
+            case 'raw':
                 break;
-	    case 'json':
-	    case 'array':
-	    case '?array':
-                if( is_array($var) ){
-                    break;//native post array
+            case 'int':
+                $var = (int) $var;
+                break;
+            case 'float':
+            case 'double':
+                $var = (float) $var;
+                break;
+            case 'bool':
+                $var = $var ? 1 : 0;
+                break;
+            case 'escape':
+            case 'string':
+                $var =  addslashes($var);
+                break;
+            case 'json':
+            case 'array':
+            case '?array':
+                if (is_array($var)) {
+                    break; //native post array
                 }
-                $var= trim($var, "\"");
-                $result= json_decode( $var,true );
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    $var=stripslashes($var);
-                    $result= json_decode( $var,true );
+                $var = trim($var, "\"");
+                $result = json_decode($var, true);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    $var = stripslashes($var);
+                    $result = json_decode($var, true);
                 }
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    throw new Exception('JSON error: '.json_last_error_msg(),500);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    throw new Exception('JSON error: ' . json_last_error_msg(), 500);
                 }
-                $var=$result;
+                $var = $result;
                 break;
             case 'object':
             case '?object':
-                $var= trim($var, "\"");
-                $result= json_decode( $var,false ); 
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    $var=stripslashes($var);
-                    $result= json_decode( $var,false );
+                $var = trim($var, "\"");
+                $result = json_decode($var, false);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    $var = stripslashes($var);
+                    $result = json_decode($var, false);
                 }
-                if( json_last_error()!=JSON_ERROR_NONE ){
-                    throw new Exception('JSON error: '.json_last_error_msg(),500);
+                if (json_last_error() != JSON_ERROR_NONE) {
+                    throw new Exception('JSON error: ' . json_last_error_msg(), 500);
                 }
-                $var=$result;
+                $var = $result;
                 break;
-	    default:
-		if( $type ){
-		    $matches=[];
-		    preg_match('/'.$type.'/u', $var, $matches);
-		    $var=  isset($matches[0])?$matches[0]:null;
-		} else {
-		    $var=  addslashes( $var );
-		}
-	}
+            default:
+                if ($type) {
+                    $matches = [];
+                    preg_match('/' . $type . '/u', $var, $matches);
+                    $var =  isset($matches[0]) ? $matches[0] : null;
+                } else {
+                    $var =  addslashes($var);
+                }
+        }
         return $var;
     }
-    public function request( $name, $type=null, $default=null ){
-	$value=$this->input->get_post($name);
-	if( !is_array($value) && strlen($value)==0 ){
-	    return $default;
-	}
-        return $this->check($value,$type);
+    public function request($name, $type = null, $default = null)
+    {
+        $value = $this->input->get_post($name);
+        if (!is_array($value) && strlen($value) == 0) {
+            return $default;
+        }
+        return $this->check($value, $type);
     }
-    public function __construct() {
-	parent::__construct();
-	$this->all_active=$this->request('all_active','bool');
-        $this->fdate=$this->request('fdate','\d\d\d\d.\d\d.\d\d');
-        $this->accounts=[
-            'active'=>[
+    public function __construct()
+    {
+        parent::__construct();
+        $this->all_active = $this->request('all_active', 'bool');
+        $this->fdate = $this->request('fdate', '\d\d\d\d.\d\d.\d\d');
+        $this->accounts = [
+            'active' => [
                 '3__'
             ],
             'passive'=>[
-                '63_',
-                '60',
-                '66'
+                '6__',
             ]
         ];
     }
-    
-    private function getAccountLeftover($acc_code,$sign=1){
-        $fdate_list=[$this->fdate];
-	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Hub->acomp('company_id');
-        $leftovers=[];
-        foreach($fdate_list as $i=>$fdate){
-            $leftovers[]="
+
+    private function getAccountLeftover($acc_code, $sign = 1)
+    {
+        $fdate_list = [$this->fdate];
+        $active_filter = $this->all_active ? '' : ' AND active_company_id=' . $this->Hub->acomp('company_id');
+        $leftovers = [];
+        foreach ($fdate_list as $i => $fdate) {
+            $leftovers[] = "
                 COALESCE((SELECT 
                         ROUND(SUM(amount) / 1000, 0)
                     FROM
@@ -119,8 +122,8 @@ class Reports_balance extends Catalog{
                         AND acc_credit_code LIKE CONCAT(acc_code,'%')
                         $active_filter),'')*$sign leftover$i";
         }
-        $leftover_queries= implode(',', $leftovers);
-        $lvl3_sql="SELECT 
+        $leftover_queries = implode(',', $leftovers);
+        $lvl3_sql = "SELECT 
                 acc_code,CONCAT(REPEAT('  ',LENGTH(acc_code)),label) acc_name,$leftover_queries
             FROM
                 acc_tree
@@ -131,21 +134,22 @@ class Reports_balance extends Catalog{
                 ";
         return $this->get_list($lvl3_sql);;
     }
-    
-    
-    private function getAccountTree($table_rows){
-        $grandtotal=0;
-        $subtotals=[];
-        foreach($table_rows as &$row){
-            $acc_code=$row->acc_code;
-            for($i=1;$i<strlen($acc_code);$i++){
-                $group_code=substr($acc_code,0,$i);
-                $subtotals[$group_code]=($subtotals[$group_code]??0)+$row->leftover0;
+
+
+    private function getAccountTree($table_rows)
+    {
+        $grandtotal = 0;
+        $subtotals = [];
+        foreach ($table_rows as &$row) {
+            $acc_code = $row->acc_code;
+            for ($i = 1; $i < strlen($acc_code); $i++) {
+                $group_code = substr($acc_code, 0, $i);
+                $subtotals[$group_code] = ($subtotals[$group_code] ?? 0) + $row->leftover0;
             }
-            $grandtotal+=$row->leftover0;
+            $grandtotal += $row->leftover0;
         }
-        foreach($subtotals as $group_code=>$subtotal){
-            $sql="
+        foreach ($subtotals as $group_code => $subtotal) {
+            $sql = "
                 SELECT
                     acc_code,CONCAT(REPEAT('  ',LENGTH(acc_code)),label) acc_name, '$subtotal' leftover0
                 FROM
@@ -153,41 +157,43 @@ class Reports_balance extends Catalog{
                 WHERE
                     acc_code='$group_code'
                 ";
-            $table_rows[]=$this->get_row($sql);
+            $table_rows[] = $this->get_row($sql);
         }
         $codes  = array_column($table_rows, 'acc_code');
         array_multisort($codes, SORT_STRING, $table_rows);
-        
+
         return (object)[
-            'total'=>$grandtotal,
-            'rows'=>$table_rows
-                ];
-    }
-    
-    private function iso2dmy( $iso ){
-	$chunks=  explode('-', $iso);
-	return "$chunks[2].$chunks[1].$chunks[0]";
-    }
-    
-    public function viewGet(){
-        $this->Hub->set_level(4);
-        
-        $active=[];
-        $passive=[];
-        foreach( $this->accounts['active'] as $acc_code ){
-            $active= array_merge($active,$this->getAccountLeftover($acc_code));
-        }
-        foreach( $this->accounts['passive'] as $acc_code ){
-            $passive= array_merge($passive,$this->getAccountLeftover($acc_code,-1));
-        }
-        $view=[
-            'input'=>[
-                'all_active'=>$this->all_active,
-                'fdate0'=>$this->iso2dmy($this->fdate)
-            ],
-            'active'=>$this->getAccountTree($active),
-            'passive'=>$this->getAccountTree($passive)
+            'total' => $grandtotal,
+            'rows' => $table_rows
         ];
-	return $view;
+    }
+
+    private function iso2dmy($iso)
+    {
+        $chunks =  explode('-', $iso);
+        return "$chunks[2].$chunks[1].$chunks[0]";
+    }
+
+    public function viewGet()
+    {
+        $this->Hub->set_level(4);
+        set_time_limit(120);
+        $active = [];
+        $passive = [];
+        foreach ($this->accounts['active'] as $acc_code) {
+            $active = array_merge($active, $this->getAccountLeftover($acc_code));
+        }
+        foreach ($this->accounts['passive'] as $acc_code) {
+            $passive = array_merge($passive, $this->getAccountLeftover($acc_code));
+        }
+        $view = [
+            'input' => [
+                'all_active' => $this->all_active,
+                'fdate0' => $this->iso2dmy($this->fdate)
+            ],
+            'active' => $this->getAccountTree($active),
+            'passive' => $this->getAccountTree($passive)
+        ];
+        return $view;
     }
 }
