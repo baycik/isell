f52825964fc58cacdf8a5048d4e4d756ba34f067
Bayci <baycikplus@gmail.com>
2026-05-05 12:42:02 +0300

    registry 2026

--- application/views/rpt/xml/registry_buy.xml.php
+++ application/views/rpt/xml/registry_buy.xml.php
@@ -1,26 +1,31 @@
 <?xml version="1.0" encoding="windows-1251"?>
-<Файл ИдФайл="<?php echo $document_name; ?>" ВерсПрог="iSell(версия 4)" ВерсФорм="5.12" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Файл ИдФайл="<?php echo $document_name; ?>" ВерсПрог="iSell(версия 6)" ВерсФорм="5.12" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 	<Документ Индекс="0000080" НомКорр="<?php echo $data['correction_num']; ?>">
 		<КнигаПокуп СумНДСВсКПк="<?php echo $data['sub_totals']->sum_total; ?>">
-                    <?php foreach($data['rows'] as $index => $item){ ?>
-			<КнПокСтр НомерПор="<?php echo $index+1; ?>" НомСчФПрод="<?php echo $item->tax_bill_num; ?>" ДатаСчФПрод="<?php echo $item->cdate; ?>" СтоимПокупВ="<?php echo abs($item->total); ?>" СумНДСВыч="<?php echo abs($item->vat); ?>">
+            <?php foreach($data['rows'] as $index => $item){ ?>
+			<КнПокСтр 
+                НомерПор="<?php echo $index+1; ?>" 
+                НомСчФПрод="<?php echo $item->tax_bill_num; ?>" 
+                ДатаСчФПрод="<?php echo $item->cdate; ?>" 
+                СтоимПокупВ="<?php echo abs($item->total); ?>" 
+                СумНДСВыч="<?php echo abs($item->vat); ?>">
 				<КодВидОпер>01</КодВидОпер>
 				<ДатаУчТов><?php echo $item->tax_bill_date; ?></ДатаУчТов>
 				<СвПрод>
-                                        <?php if(strlen($item->company_tax_id) == 12){ ?>
-					<СведИП ИННФЛ="<?php echo $item->company_tax_id; ?>"/>
-                                         <?php } else { ?>  
-                                        <СведЮЛ ИННЮЛ="<?php echo $item->company_tax_id; ?>" КПП="<?php echo $item->company_tax_id2; ?>" />
-                                         <?php } ?>    
-				</СвПрод>
-                                <?php if($item->party_labels){ ?>
-                                    <?php foreach(explode(',', $item->party_labels) as $party_label){ ?>
-                                        <?php if(!empty($party_label) && strlen($party_label) < 22 && strlen($party_label) < 28){ ?>
-                                    <РегНомТД><?php echo trim(preg_replace('/[\t|\s{2,}]/', '', $party_label)); ?></РегНомТД>
-                                    <?php }} ?>  
-                                <?php } ?>   
-			</КнПокСтр>
+                    <?php if(strlen($item->company_tax_id) == 12){ ?>
+					    <СведИП ИННФЛ="<?php echo $item->company_tax_id; ?>"/>
+                    <?php } else { ?>  
+                        <СведЮЛ ИННЮЛ="<?php echo $item->company_tax_id; ?>" КПП="<?php echo $item->company_tax_id2; ?>" />
                     <?php } ?>    
+				</СвПрод>
+                    <?php if($item->party_labels){ ?>
+                        <?php foreach(explode(',', $item->party_labels) as $party_label){ ?>
+                            <?php if(!empty($party_label) && strlen($party_label) < 22 && strlen($party_label) < 28){ ?>
+                        <РегНомТД><?php echo trim(preg_replace('/[\t|\s{2,}]/', '', $party_label)); ?></РегНомТД>
+                        <?php }} ?>  
+                    <?php } ?>   
+			</КнПокСтр>
+            <?php } ?>    
 		</КнигаПокуп>
 	</Документ>
 </Файл>
