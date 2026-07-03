17df72da7cb14215f5d027e6f58042437802ed64
Bayci <baycikplus@gmail.com>
2026-07-03 15:12:10 +0300

    small fixes

--- index.php
+++ index.php
@@ -66,8 +66,8 @@ include "../config.php";
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-
+	//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', 'production');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
